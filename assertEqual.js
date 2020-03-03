3b263fed82bf236c5ce7869240efad47bc5b1785
Elizabeth Brown <ej2brown@edu.uwaterloo.ca>
2020-03-03 14:23:53 +0000

    finished assignment final submittion

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual("one","one"));
+console.log(assertEqual(1, 3));
---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function (arr) {
+  console.log(arr.slice[-1]);
+  for (let i = 1; i < arr.length; i++) {
+    stringCon += arr[i];
+  }
+}
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
